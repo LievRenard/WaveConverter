b6f686f79952138ce72105778a5602d48381baa8
LievRenard <emevanpoivel@gmail.com>
2023-09-14 13:16:20 +0900

    add help page and linear wavenumber

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ class Program {
         double c = 3e8;
 
         string cmd = string.Join(' ',args);
-        string pattern = "([0-9])?[.]?[0-9]+([eE][+-]?[0-9]+)? *((nm)|(rad[/]s)|(Hz)|(rad[/]cm)|(eV))";
+        string pattern = "([0-9])?[.]?[0-9]+([eE][+-]?[0-9]+)? *((nm)|(um)|(rad[/]s)|(Hz)|(THz)|(rad[/]cm)|(rad[/]um)|(cm-1)|(eV))";
+
+        if (cmd == "--help" || cmd == "-h") {
+            Console.WriteLine(@"
+WaveConverter(conv) Usage:
+
+conv <value> <unit>
+
+Converts value with specific unit to other electromagnetic wave properties. Input property is decided by input unit.
+Value and unit can be seperated by blank or not. Capital letter must be distinguished in unit.
+
+Support properties and unit:
+
+Wavelength - nm, um
+Frequency - Hz, THz
+Angular Frequency - rad/s
+Angular Wavenumber - rad/cm, rad/um
+Linear Wavenumber - cm-1
+Photon Energy - eV
+            ");
+            return 0;
+        }
 
         if (!Regex.IsMatch(cmd, pattern)) {
             Console.WriteLine("Command Cannot Be Interpreted.");
@@ -14,7 +35,7 @@ class Program {
         }
 
         cmd = Regex.Match(cmd,pattern).Value;
-        string unit = Regex.Match(cmd,"(nm)|(rad[/]s)|(Hz)|(rad[/]cm)|(eV)").Value;
+        string unit = Regex.Match(cmd,"(nm)|(um)|(rad[/]s)|(Hz)|(THz)|(rad[/]cm)|(rad[/]um)|(cm-1)|(eV)").Value;
         double val = double.Parse(Regex.Match(cmd,"([0-9])?[.]?[0-9]+([eE][+-]?[0-9]+)?").Value);
         switch (unit)
         {
@@ -23,8 +44,29 @@ class Program {
                 val *= 1e-9;
                 Console.WriteLine("f = {0:e2} Hz",c/val);
                 Console.WriteLine("ω = {0:e2} rad/s",2*pi*c/val);
-                if (2*pi/val/100 < 5000) {Console.WriteLine("k = {0:f2} rad/cm",2*pi/val/100);}
-                else {Console.WriteLine("k = {0:f2} rad/μm",2*pi/val/1e6);}
+                if (2*pi/val/100 < 50000) {
+                    Console.WriteLine("k = {0:f2} rad/cm",2*pi/val/100);
+                    Console.WriteLine(" or {0:f2} cm-1",1/val/100);
+                    }
+                else {
+                    Console.WriteLine("k = {0:f2} rad/μm",2*pi/val/1e6);
+                    Console.WriteLine(" or {0:f2} μm-1",1/val/1e6);
+                    }
+                Console.WriteLine("E = {0:f2} eV",1239.8/val/1e9);
+                break;
+            case "um":
+                Console.WriteLine("Converted :");
+                val *= 1e-6;
+                Console.WriteLine("f = {0:e2} Hz",c/val);
+                Console.WriteLine("ω = {0:e2} rad/s",2*pi*c/val);
+                if (2*pi/val/100 < 50000) {
+                    Console.WriteLine("k = {0:f2} rad/cm",2*pi/val/100);
+                    Console.WriteLine(" or {0:f2} cm-1",1/val/100);
+                    }
+                else {
+                    Console.WriteLine("k = {0:f2} rad/μm",2*pi/val/1e6);
+                    Console.WriteLine(" or {0:f2} μm-1",1/val/1e6);
+                    }
                 Console.WriteLine("E = {0:f2} eV",1239.8/val/1e9);
                 break;
             case "rad/s":
@@ -32,8 +74,14 @@ class Program {
                 if (2*pi*c/val*1e9 < 2000) {Console.WriteLine("λ = {0:f2} nm",2*pi*c/val*1e9);}
                 else {Console.WriteLine("λ = {0:f2} μm",2*pi*c/val*1e6);}
                 Console.WriteLine("f = {0:e2} Hz",val/2/pi);
-                if (val/c/100 < 5000) {Console.WriteLine("k = {0:f2} rad/cm",val/c/100);}
-                else {Console.WriteLine("k = {0:f2} rad/μm",val/c/1e6);}
+                if (val/c/100 < 50000) {
+                    Console.WriteLine("k = {0:f2} rad/cm",val/c/100);
+                    Console.WriteLine(" or {0:f2} cm-1",val/c/2/pi/100);
+                    }
+                else {
+                    Console.WriteLine("k = {0:f2} rad/μm",val/c/1e6);
+                    Console.WriteLine(" or {0:f2} μm-1",val/c/2/pi/1e6);
+                    }
                 Console.WriteLine("E = {0:f2} eV",1239.8/(2*pi*c/val*1e9));
                 break;
             case "Hz":
@@ -41,8 +89,30 @@ class Program {
                 if (c/val*1e9 < 2000) {Console.WriteLine("λ = {0:f2} nm",c/val*1e9);}
                 else {Console.WriteLine("λ = {0:f2} μm",c/val*1e6);}
                 Console.WriteLine("ω = {0:e2} rad/s",val*2*pi);
-                if (2*pi*val/c/100 < 5000) {Console.WriteLine("k = {0:f2} rad/cm",2*pi*val/c/100);}
-                else {Console.WriteLine("k = {0:f2} rad/μm",2*pi*val/c/1e6);}
+                if (2*pi*val/c/100 < 50000) {
+                    Console.WriteLine("k = {0:f2} rad/cm",2*pi*val/c/100);
+                    Console.WriteLine(" or {0:f2} cm-1",1*val/c/100);
+                    }
+                else {
+                    Console.WriteLine("k = {0:f2} rad/μm",2*pi*val/c/1e6);
+                    Console.WriteLine(" or {0:f2} μm-1",1*val/c/1e6);
+                    }
+                Console.WriteLine("E = {0:f2} eV",1239.8/(c/val*1e9));
+                break;
+            case "THz":
+                Console.WriteLine("Converted :");
+                val *= 1e12;
+                if (c/val*1e9 < 2000) {Console.WriteLine("λ = {0:f2} nm",c/val*1e9);}
+                else {Console.WriteLine("λ = {0:f2} μm",c/val*1e6);}
+                Console.WriteLine("ω = {0:e2} rad/s",val*2*pi);
+                if (2*pi*val/c/100 < 50000) {
+                    Console.WriteLine("k = {0:f2} rad/cm",2*pi*val/c/100);
+                    Console.WriteLine(" or {0:f2} cm-1",1*val/c/100);
+                    }
+                else {
+                    Console.WriteLine("k = {0:f2} rad/μm",2*pi*val/c/1e6);
+                    Console.WriteLine(" or {0:f2} μm-1",1*val/c/1e6);
+                    }
                 Console.WriteLine("E = {0:f2} eV",1239.8/(c/val*1e9));
                 break;
             case "rad/cm":
@@ -52,6 +122,27 @@ class Program {
                 else {Console.WriteLine("λ = {0:f2} μm",2*pi/val*1e6);}
                 Console.WriteLine("f = {0:e2} Hz",c*val/2/pi);
                 Console.WriteLine("ω = {0:e2} rad/s",c*val);
+                Console.WriteLine("k(lin) = {0:f2} cm-1",val/2/pi/1e2);
+                Console.WriteLine("E = {0:f2} eV",1239.8/(2*pi/val*1e9));
+                break;
+            case "rad/um":
+                Console.WriteLine("Converted :");
+                val *= 1e6;
+                if (2*pi/val*1e9 < 2000) {Console.WriteLine("λ = {0:f2} nm",2*pi/val*1e9);}
+                else {Console.WriteLine("λ = {0:f2} μm",2*pi/val*1e6);}
+                Console.WriteLine("f = {0:e2} Hz",c*val/2/pi);
+                Console.WriteLine("ω = {0:e2} rad/s",c*val);
+                Console.WriteLine("k(lin) = {0:f2} μm-1",val/2/pi/1e6);
+                Console.WriteLine("E = {0:f2} eV",1239.8/(2*pi/val*1e9));
+                break;
+            case "cm-1":
+                Console.WriteLine("Converted :");
+                val *= 1e2*2*pi;
+                if (2*pi/val*1e9 < 2000) {Console.WriteLine("λ = {0:f2} nm",2*pi/val*1e9);}
+                else {Console.WriteLine("λ = {0:f2} μm",2*pi/val*1e6);}
+                Console.WriteLine("f = {0:e2} Hz",c*val/2/pi);
+                Console.WriteLine("ω = {0:e2} rad/s",c*val);
+                Console.WriteLine("k(ang) = {0:f2} cm-1",val/1e2);
                 Console.WriteLine("E = {0:f2} eV",1239.8/(2*pi/val*1e9));
                 break;
             case "eV":
@@ -61,8 +152,14 @@ class Program {
                 val = 1239.8/val*1e-9;
                 Console.WriteLine("f = {0:e2} Hz",c/val);
                 Console.WriteLine("ω = {0:e2} rad/s",2*pi*c/val);
-                if (2*pi/val/100 < 5000) {Console.WriteLine("k = {0:f2} rad/cm",2*pi/val/100);}
-                else {Console.WriteLine("k = {0:f2} rad/μm",2*pi/val/1e6);}
+                if (2*pi/val/100 < 50000) {
+                    Console.WriteLine("k = {0:f2} rad/cm",2*pi/val/100);
+                    Console.WriteLine(" or {0:f2} cm-1",1/val/100);
+                    }
+                else {
+                    Console.WriteLine("k = {0:f2} rad/μm",2*pi/val/1e6);
+                    Console.WriteLine(" or {0:f2} μm-1",1/val/1e6);
+                    }
                 break;
             default:
                 Console.WriteLine("Command Cannot Be Interpreted.");
--- WaveConverterInstaller.ps1
+++ WaveConverterInstaller.ps1
@@ -1,13 +1,9 @@
-Write-Output "Install WaveConverter..."
-if (Test-Path .\WaveConverter\) {
-    Remove-Item .\WaveConverter\ -Recurse
-}
-Invoke-WebRequest -Uri https://github.com/LievRenard/WaveConverter/releases/download/release/WaveConverter.zip -OutFile .\WaveConverter.zip
-Expand-Archive .\WaveConverter.zip
-Remove-Item .\WaveConverter.zip
+Write-Output "Setting alias in Powershell profile..."
 if (!(Test-Path $profile)) {
+    Write-Output "Powershell profile not found. Making new profile..."
     New-Item -Path $profile -ItemType "file" -Force
+    Write-Output "Powershell profile created."
 }
-$command = "Set-Alias conv" + (Get-Location).Path + "\WaveConverter\WaveConverter.exe"
+$command = "Set-Alias conv " + (Get-Location).Path + "\WaveConverter\WaveConverter.exe"
 Add-Content -Path $profile -Value $command
-Write-Output "Installation end. Please restart terminal."
+Write-Output "Setting end. Please restart terminal."
